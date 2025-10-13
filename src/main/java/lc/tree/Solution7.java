fb3ca176090ae4cf677a38b017bff0f4248c7c3a
SDB <db7734@naver.com>
2025-10-13 22:32:11 +0900

    leet code 이진 트리 문제

---
+++ src/main/java/lc/tree/Solution7.java
@@ -0,0 +1,19 @@
+package main.java.lc.tree;
+
+public class Solution7 {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        targetSum = targetSum - root.val;
+
+        if (root.left == null && root.right == null) {
+            return targetSum == 0;
+        }
+
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    }
+
+}
